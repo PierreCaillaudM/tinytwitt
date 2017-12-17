e1d098c537b639339a95e17e69f6fe67a7a32ab0
Nikuto <callof61@gmail.com>
2017-12-17 05:55:22 +0100

    Un peu de css et debut d'angular

--- war/script.js
+++ war/script.js
@@ -1,13 +1,33 @@
 var app = angular.module('twitter',[]).controller('TController',['$scope','$window',
  function($scope, $window){
 	
+	$scope.twitt = [{author: 'admin',message :'Bienvenu sur tiny twitt'}];
+	$scope.sauthor;
+	$scope.smessage;
+	$scope.login = 'null';
+	
+	$scope.connection = function(nom){
+		$scope.login = nom;
+		console.log($scope.login);
+	}
+	
+	$scope.listeTwitt = function(){
+		if($scope.login != 'null'){
+			gapi.client.TinytwittEndpoint.getTweetsOf($scope.login).execute(
+					function(resp){
+						$scope.twitt = resp.items;
+						$scope.$apply();
+						console.log(resp);
+					}
+			);
+		}
+	}
 	
 	$window.init = function() {
 	      console.log("windowinit called");
-	      var rootApi = 'https://1-dot-sobike44.appspot.com/_ah/api/';
-	      gapi.client.load('scoreentityendpoint', 'v1', function() {
-	        console.log("score api loaded");
-	        $scope.listScore();
+	      var rootApi = 'https://tiny-twitt.appspot.com/_ah/api/';
+	      gapi.client.load('TinytwittEndpoint', 'v1', function() {
+	        console.log("twitt api loaded");
 	      }, rootApi);
 	 }
  }                                                             
